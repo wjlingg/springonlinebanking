74bb94c5c1da1add9fb53aef1ca4f3ea7a39def1
wjlingg <e0309488@u.nus.edu>
2022-11-22 17:50:54 +0800

    First push

---
+++ src/main/java/com/uob/springonlinebanking/SpringOnlineBankingApplication.java
@@ -0,0 +1,13 @@
+package com.uob.springonlinebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOnlineBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOnlineBankingApplication.class, args);
+	}
+
+}
