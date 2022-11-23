d74d8b46cb3651f7e8c15aeaa3aea07a842ecb84
wjlingg <e0309488@u.nus.edu>
2022-11-23 18:27:47 +0800

    Added auto increment sequence generator

--- src/main/java/com/uob/springonlinebanking/models/Accounts.java
+++ src/main/java/com/uob/springonlinebanking/models/Accounts.java
@@ -1,31 +1,21 @@
 package com.uob.springonlinebanking.models;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 public class Accounts {
-
-    @Id
-    @GenericGenerator(
-	    name = "account-sequence-generator",
-	    strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
-	    parameters = {
-	            @Parameter(name = "sequence_name", value = "account_sequence"),
-	            @Parameter(name = "initial_value", value = "1000000000"),
-	            @Parameter(name = "increment_size", value = "1")
-	    })
-	@GeneratedValue(generator = "account-sequence-generator")
+	@Id
+	@GeneratedValue(generator = "id_seq", strategy = GenerationType.IDENTITY)
+	@SequenceGenerator(name="id_seq", sequenceName="id_sequence", initialValue=1000000000, allocationSize=1)
 	private long accountId;
     
     private String accountType;
