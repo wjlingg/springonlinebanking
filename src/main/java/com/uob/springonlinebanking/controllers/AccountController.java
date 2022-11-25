64bcd32843d11f3fb81483c8b0c153f5bd230dce
Benedict <benedictwcy@hotmail.com>
2022-11-25 16:11:57 +0800

    Added getUserId method in MyUserDetails

--- src/main/java/com/uob/springonlinebanking/controllers/AccountController.java
+++ src/main/java/com/uob/springonlinebanking/controllers/AccountController.java
@@ -1,18 +1,14 @@
 package com.uob.springonlinebanking.controllers;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.support.RequestContextUtils;
-
 import com.uob.springonlinebanking.models.Accounts;
 import com.uob.springonlinebanking.models.Users;
 import com.uob.springonlinebanking.repositories.AccountRepository;
@@ -20,12 +16,12 @@ import com.uob.springonlinebanking.repositories.UserRepository;
 
 @Controller
 public class AccountController {
-	
 	@Autowired
 	AccountRepository accountRepo;
-	
 	@Autowired
 	UserRepository userRepo;
+	
+	private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 	
 	@GetMapping("/")
 	public String showMain() {
@@ -41,8 +37,10 @@ public class AccountController {
 	}
 	
 	@PostMapping("/process_register")
-	public String processRegister(@RequestParam("accountType") String accountType, Users user, Model model, RedirectAttributes redirectAttributes) {
-		
+	public String processRegister(@RequestParam("accountType") String accountType, Users user) {
+		if(!StringUtils.isEmpty(user.getPassword())) {
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
+		}
 		userRepo.save(user); // save to user repository
 		Users userLocal = userRepo.findUserIdByNric(user.getUserNric()); // get the user that has been just saved
 		Accounts account = new Accounts(accountType, 0.0, userLocal); // create account with the saved user
@@ -53,7 +51,7 @@ public class AccountController {
 	
 	
 	@GetMapping("/welcomeuser")
-	public String welcomeUser(Users user, Model model) {
+	public String welcomeUser() {
 		return "welcomeUser"; // render welcomeUser.html
 	}
 }
